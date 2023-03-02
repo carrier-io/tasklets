ffdda1972223cdd2f9e0920e5b80ea2e021f5dde
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2023-03-02 19:52:26 +0200

    Explicit BigInt conversion

--- static/js/run_logs.js
+++ static/js/run_logs.js
@@ -18,7 +18,7 @@ const RunLogsApp = {
     mounted() {
       this.state = 'initializing'
       this.logs_pull_end = BigInt(this.logs_ts_now)
-      this.logs_tail_ts = BigInt(this.logs_ts_now) + 1
+      this.logs_tail_ts = BigInt(this.logs_ts_now) + BigInt(1)
       this.init_websocket()
       console.log("TS #1: ", this.logs_ts_now)
       console.log("TS #2: ", this.logs_pull_end)
